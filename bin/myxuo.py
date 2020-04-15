b15b4b6956ad8ede06a47e2af56410f013172ba6
Twilight Sparkle <twilight_princess_sparkle@outlook.com>
2020-04-15 21:10:54 +0800

    Normal Develop

--- bin/myxuo.py
+++ bin/myxuo.py
@@ -26,36 +26,68 @@ import urllib2
 import requests
 import random
 import xml.etree.cElementTree as Et
+import ConfigParser as Cp
 import os
 import sys
+from ast import literal_eval
+
+MY_COLORS = {}
 
 
-class Img:
-    def __init__(self, path):
-        self.img = pygame.image.load(path)
-        self.rect = self.img.get_rect()
-        self.width = self.rect.width
-        self.height = self.rect.height
+def start_page(screen):
+    screen.fill(get_colors(Et.parse(
+        '../res/xml/style.xml').getroot().find('pages').find('start').find('bg_color').text))
+    xuo_logo_white = pygame.image.load('../res/assets/XUOIcon-White.png')
+    xuo_logo_white_rect = xuo_logo_white.get_rect()
+    xuo_logo_white_rect.center = screen.get_rect().center
+    screen.blit(xuo_logo_white, xuo_logo_white_rect)
 
-    def cal_position(self, center):
-        width = self.width
-        height = self.height
-        return [center[0] - (width / 2), center[1] - (height / 2)]
+
+def append_colors(colors, path):
+    try:
+        reader = Cp.ConfigParser()
+        reader.read(path)
+        items = reader.items('colors')
+        for i in items:
+            if not i[0] in colors:
+                colors[i[0]] = literal_eval(i[1])
+    except Cp.Error:
+        pass
+
+
+def get_colors(color):
+    try:
+        return MY_COLORS[color][:]
+    except KeyError:
+        return [0, 0, 0, 0]
+
+
+def switch_page(screen, page):
+    pass
 
 
 if __name__ == '__main__':
     pygame.init()
     pygame.mixer.init()
     # Init pygame
-    screen = pygame.display.set_mode([640, 480])
-    screen.fill([11, 187, 255])
-    pygame.display.set_caption('MyXUO')
-    # Init screen
 
     current_window = 'start'
-    xuo_logo_white = Img('../res/assets/XUOIcon-White.png')
-    screen.blit(xuo_logo_white.img, xuo_logo_white.cal_position([320, 240]))
 
+    append_colors(MY_COLORS, '../res/raw/art.ini')
+
+    # Load files
+    screen_main = pygame.display.set_mode([640, 480])
+    screen_main_rect = screen_main.get_rect()
+    alpha_main = screen_main.convert_alpha()
+    alpha_main_rect = alpha_main.get_rect()
+
+    pygame.display.set_caption('MyXUO')
+    # Create screens
+
+    # start page
+
+    start_page(alpha_main)
+    screen_main.blit(alpha_main, [0, 0])
     running = True
 
     while running:
@@ -65,3 +97,4 @@ if __name__ == '__main__':
 
         pygame.display.update()
     pygame.quit()
+sys.exit(0)
